a993f0fe5277e856e8f21b0a866cb56e0874126b
Mitama Masuda <mitama.md@gmail.com>
2022-08-12 20:55:50 +0900

    Implement linked lists functions to value_n_from_end() in C

---
+++ linked_lists/linked_lists.h
@@ -0,0 +1,38 @@
+#ifndef LINKED_LISTS_H
+#define LINKED_LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum bool { FALSE, TRUE } bool;
+
+typedef struct	s_listnode
+{
+	int data;
+	struct s_listnode *next;
+}				t_listnode;
+
+typedef struct	s_list
+{
+	size_t size;
+	struct s_listnode *head;
+	struct s_listnode *tail;
+}				t_list;
+
+t_list *list_new(void);
+void list_destroy(t_list *list);
+t_listnode *listnode_new(const int data, t_listnode *next);
+size_t list_size(const t_list *list);
+bool list_is_empty(const t_list *list);
+int list_value_at(const t_list *list, const size_t index);
+void list_push_front(t_list *list, int data);
+int list_pop_front(t_list *list);
+void list_push_back(t_list *list, int data);
+int list_pop_back(t_list *list);
+int list_front(t_list *list);
+int list_back(t_list *list);
+void list_insert(t_list *list, size_t index, int data);
+void list_erase(t_list *list, size_t index);
+int list_value_n_from_end(t_list *list, size_t n);
+
+#endif
